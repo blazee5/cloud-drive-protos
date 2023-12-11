6031ee0dcfd08d0f652ec6426bf39721f7f7291b
blazee5 <vladislavkutsenko742@gmail.com>
2023-12-11 21:48:12 +0600

    remove user_id from validate and change name

--- auth/auth_grpc.pb.go
+++ auth/auth_grpc.pb.go
@@ -19,9 +19,9 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	AuthService_SignUp_FullMethodName       = "/auth.AuthService/SignUp"
-	AuthService_SignIn_FullMethodName       = "/auth.AuthService/SignIn"
-	AuthService_ValidateCode_FullMethodName = "/auth.AuthService/ValidateCode"
+	AuthService_SignUp_FullMethodName          = "/auth.AuthService/SignUp"
+	AuthService_SignIn_FullMethodName          = "/auth.AuthService/SignIn"
+	AuthService_ValidateAccount_FullMethodName = "/auth.AuthService/ValidateAccount"
 )
 
 // AuthServiceClient is the client API for AuthService service.
@@ -30,7 +30,7 @@ const (
 type AuthServiceClient interface {
 	SignUp(ctx context.Context, in *SignUpRequest, opts ...grpc.CallOption) (*UserResponse, error)
 	SignIn(ctx context.Context, in *SignInRequest, opts ...grpc.CallOption) (*Token, error)
-	ValidateCode(ctx context.Context, in *ValidateCodeRequest, opts ...grpc.CallOption) (*ValidateCodeResponse, error)
+	ValidateAccount(ctx context.Context, in *ValidateAccountRequest, opts ...grpc.CallOption) (*ValidateAccountResponse, error)
 }
 
 type authServiceClient struct {
@@ -59,9 +59,9 @@ func (c *authServiceClient) SignIn(ctx context.Context, in *SignInRequest, opts 
 	return out, nil
 }
 
-func (c *authServiceClient) ValidateCode(ctx context.Context, in *ValidateCodeRequest, opts ...grpc.CallOption) (*ValidateCodeResponse, error) {
-	out := new(ValidateCodeResponse)
-	err := c.cc.Invoke(ctx, AuthService_ValidateCode_FullMethodName, in, out, opts...)
+func (c *authServiceClient) ValidateAccount(ctx context.Context, in *ValidateAccountRequest, opts ...grpc.CallOption) (*ValidateAccountResponse, error) {
+	out := new(ValidateAccountResponse)
+	err := c.cc.Invoke(ctx, AuthService_ValidateAccount_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -74,7 +74,7 @@ func (c *authServiceClient) ValidateCode(ctx context.Context, in *ValidateCodeRe
 type AuthServiceServer interface {
 	SignUp(context.Context, *SignUpRequest) (*UserResponse, error)
 	SignIn(context.Context, *SignInRequest) (*Token, error)
-	ValidateCode(context.Context, *ValidateCodeRequest) (*ValidateCodeResponse, error)
+	ValidateAccount(context.Context, *ValidateAccountRequest) (*ValidateAccountResponse, error)
 	mustEmbedUnimplementedAuthServiceServer()
 }
 
@@ -88,8 +88,8 @@ func (UnimplementedAuthServiceServer) SignUp(context.Context, *SignUpRequest) (*
 func (UnimplementedAuthServiceServer) SignIn(context.Context, *SignInRequest) (*Token, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method SignIn not implemented")
 }
-func (UnimplementedAuthServiceServer) ValidateCode(context.Context, *ValidateCodeRequest) (*ValidateCodeResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method ValidateCode not implemented")
+func (UnimplementedAuthServiceServer) ValidateAccount(context.Context, *ValidateAccountRequest) (*ValidateAccountResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ValidateAccount not implemented")
 }
 func (UnimplementedAuthServiceServer) mustEmbedUnimplementedAuthServiceServer() {}
 
@@ -140,20 +140,20 @@ func _AuthService_SignIn_Handler(srv interface{}, ctx context.Context, dec func(
 	return interceptor(ctx, in, info, handler)
 }
 
-func _AuthService_ValidateCode_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(ValidateCodeRequest)
+func _AuthService_ValidateAccount_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ValidateAccountRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(AuthServiceServer).ValidateCode(ctx, in)
+		return srv.(AuthServiceServer).ValidateAccount(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: AuthService_ValidateCode_FullMethodName,
+		FullMethod: AuthService_ValidateAccount_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(AuthServiceServer).ValidateCode(ctx, req.(*ValidateCodeRequest))
+		return srv.(AuthServiceServer).ValidateAccount(ctx, req.(*ValidateAccountRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -174,8 +174,8 @@ var AuthService_ServiceDesc = grpc.ServiceDesc{
 			Handler:    _AuthService_SignIn_Handler,
 		},
 		{
-			MethodName: "ValidateCode",
-			Handler:    _AuthService_ValidateCode_Handler,
+			MethodName: "ValidateAccount",
+			Handler:    _AuthService_ValidateAccount_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
